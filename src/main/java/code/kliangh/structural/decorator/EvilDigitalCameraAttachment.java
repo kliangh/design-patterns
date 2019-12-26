37fe01fbc97630e18c572a44556dec675bf7c8d4
kliangh <kunliang.hou@gmail.com>
2019-12-26 13:40:29 +0800

    design-patterns. designed decorator class. 1. created an abstract class, EvilDigitalCameraAttachment, for EvilDigitalCamera

--- src/main/java/code/kliangh/structural/decorator/EvilDigitalCameraAttachment.java
+++ src/main/java/code/kliangh/structural/decorator/EvilDigitalCameraAttachment.java
@@ -1,4 +1,19 @@
 package code.kliangh.structural.decorator;
 
-public class EvilDigitalCameraAttachment {
+public abstract class EvilDigitalCameraAttachment implements EvilDigitalCamera {
+    final EvilDigitalCamera targetEvilDigitalCamera;
+
+    public EvilDigitalCameraAttachment(EvilDigitalCamera evilDigitalCamera) {
+        this.targetEvilDigitalCamera = evilDigitalCamera;
+    }
+
+    @Override
+    public ExifInformation shoot() {
+        return targetEvilDigitalCamera.shoot();
+    }
+
+    @Override
+    public StringBuilder getEvilDigitalCameraLoadout() {
+        return targetEvilDigitalCamera.getEvilDigitalCameraLoadout();
+    }
 }
