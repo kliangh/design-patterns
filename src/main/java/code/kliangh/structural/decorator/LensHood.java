7aa8d1047b8bdcd0a643034107809e8d2c9ab409
kliangh <kunliang.hou@gmail.com>
2019-12-26 16:23:32 +0800

    design-patterns. implementing decorators. 1. implemented decorators, LendHood & VerticalGrip

--- src/main/java/code/kliangh/structural/decorator/LensHood.java
+++ src/main/java/code/kliangh/structural/decorator/LensHood.java
@@ -1,4 +1,27 @@
 package code.kliangh.structural.decorator;
 
-public class LensHood {
+public class LensHood extends EvilDigitalCameraAttachment {
+    public LensHood(EvilDigitalCamera evilDigitalCamera) {
+        super(evilDigitalCamera);
+    }
+
+    @Override
+    public void initializeCameraProperty(DigitalCameraProperties digitalCameraProperties) {
+        targetEvilDigitalCamera.initializeCameraProperty(digitalCameraProperties);
+    }
+
+    @Override
+    public ExifInformation shoot() {
+        this.initializeCameraProperty(getDigitalCameraProperties());
+        return super.shoot();
+    }
+
+    @Override
+    public StringBuilder getEvilDigitalCameraLoadout() {
+        return super.getEvilDigitalCameraLoadout().append(", lens hood attached");
+    }
+
+    private DigitalCameraProperties getDigitalCameraProperties() {
+        return new DigitalCameraProperties(-0.5, null);
+    }
 }
--- src/main/java/code/kliangh/structural/decorator/VerticalGrip.java
+++ src/main/java/code/kliangh/structural/decorator/VerticalGrip.java
@@ -1,4 +1,27 @@
 package code.kliangh.structural.decorator;
 
-public class VerticalGrip {
+public class VerticalGrip extends EvilDigitalCameraAttachment {
+    public VerticalGrip(EvilDigitalCamera evilDigitalCamera) {
+        super(evilDigitalCamera);
+        initializeCameraProperty(getDigitalCameraProperties());
+    }
+
+    @Override
+    public void initializeCameraProperty(DigitalCameraProperties digitalCameraProperties) {
+        targetEvilDigitalCamera.initializeCameraProperty(digitalCameraProperties);
+    }
+
+    @Override
+    public ExifInformation shoot() {
+        return super.shoot();
+    }
+
+    @Override
+    public StringBuilder getEvilDigitalCameraLoadout() {
+        return super.getEvilDigitalCameraLoadout().append(", attached vertical grip");
+    }
+
+    private DigitalCameraProperties getDigitalCameraProperties() {
+        return new DigitalCameraProperties(null, +200);
+    }
 }
